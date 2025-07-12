8a465e1840efd6eb32f7c1d6ebc6827e00c32fbc
Brandon Espina <brandon.r.espina@gmail.com>
2025-07-12 15:15:45 -0500

    beginnings of oauth

--- lesson-3-foundations-of-authenticating-integrations/02-the-basics-of-the-oauth-flow/index.js
+++ lesson-3-foundations-of-authenticating-integrations/02-the-basics-of-the-oauth-flow/index.js
@@ -13,7 +13,7 @@ const CLIENT_SECRET = process.env.CLIENT_SECRET;
 
 const REDIRECT_URI = `http://localhost:3000/oauth-callback`;
 
-const authUrl = ``;
+const authUrl = `https://app-na2.hubspot.com/oauth/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&scope=crm.objects.contacts.write%20oauth%20crm.objects.contacts.read`;
 
 const tokenStore = {};
 
@@ -28,5 +28,14 @@ app.use(session({
 // * 3. Combine temporary auth code wtih app credentials and send back to OAuth server.
 // * 4. Get access and refresh tokens.
 
+
+// * 1. Send user to authorization page. This kicks off initial requeset to OAuth server.
+app.get('/', async (req, res) => {
+    if () {
+
+    } else {
+        res.render("home", { authUrl });
+    }
+});
 app.listen(3000, () => console.log('App running here: http://localhost:3000'));
 
