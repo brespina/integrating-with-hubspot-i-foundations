7f472788576eed2fffad8880d9061c0a8dcdd59d
Brandon Espina <brandon.r.espina@gmail.com>
2025-07-14 14:35:38 -0500

    tried to postman, removed example accessToken, gave dotenv to package.json

--- lesson-3-foundations-of-authenticating-integrations/05-the-basics-of-using-a-private-app/finished/index.js
+++ lesson-3-foundations-of-authenticating-integrations/05-the-basics-of-using-a-private-app/finished/index.js
@@ -2,7 +2,8 @@ const express = require('express');
 const axios = require('axios');
 const app = express();
 
-const PRIVATE_APP_ACCESS = `pat-na1-b84163a6-1d98-44fd-a0e2-aaceefa3edc8`;
+// removed hubspot example of access token, should use .env
+const PRIVATE_APP_ACCESS = ``;
 
 app.get('/', async (req, res) => {
     const companies = 'https://api.hubspot.com/crm/v3/objects/companies';
--- lesson-3-foundations-of-authenticating-integrations/05-the-basics-of-using-a-private-app/index.js
+++ lesson-3-foundations-of-authenticating-integrations/05-the-basics-of-using-a-private-app/index.js
@@ -1,7 +1,24 @@
+require('dotenv').config()
 const express = require('express');
 const axios = require('axios');
 const app = express();
 
-const PRIVATE_APP_ACCESS = ``;
+// fails bc no dotenv module
+const POSTMAN_PRIVATE_APP_ACCESS = process.env.POSTMAN_PRIVATE_APP_ACCESS
+console.log(POSTMAN_PRIVATE_APP_ACCESS)
+
+app.get('/', async (req, res) => {
+    const companies = 'https://api.hubspot.com/crm/v3/objects/companies';
+    const headers = {
+        Authorization: `Bearer ${POSTMAN_PRIVATE_APP_ACCESS}`,
+        'Content-Type': 'application/json'
+    }
+    try {
+        const response = await axios.get(companies, { headers });
+        res.json(response.data.results);    
+    } catch (error) {
+        console.error(error);
+    }
+});
 
 app.listen(3000, () => console.log('Listening on http://localhost:3000'));
